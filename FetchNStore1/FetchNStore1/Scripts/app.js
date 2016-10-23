c448fba759bef71dcb0c54ac009c54e480461f61
Scott Schaffer <scottpschaffer@hotmail.com>
2016-10-22 19:05:53 -0700

    Finished Adding controls to view. Having trouble with AJAX

--- FetchNStore1/FetchNStore1/Scripts/app.js
+++ FetchNStore1/FetchNStore1/Scripts/app.js
@@ -1,1 +1,14 @@
-﻿var app = angular.module("FNS", []);
+﻿var app = angular.module("FNS", [])
+    .controller('myController', function ($scope, $http) {
+
+        $scope.fetch = function(){
+            //$scope.data.qwert = 'qqaazzzxxsswwwww';
+            $http({
+                method : "GET",
+                url : "http://httpstat.us/200"
+            }).then(function successCallBack(response) {
+                $scope.data.resp = response.data,
+                $scope.data.qwert = response.headers;
+            });
+        };
+    });
