c6cae8df01ae1f94f7f60356c5b1ea158402cd98
Scott Schaffer <scottpschaffer@hotmail.com>
2016-10-24 18:54:26 -0700

    Added Response Time and Entity Framework

--- FetchNStore1/FetchNStore1/Scripts/app.js
+++ FetchNStore1/FetchNStore1/Scripts/app.js
@@ -2,12 +2,15 @@
     .controller('myController', function ($scope, $http) {
 
         $scope.fetch = function () {
+            var time = Date.now();
             $scope.data.resTime = "sssss";
             $http({
                 method: $scope.data.singleMethod,
                 url : $scope.data.theURL
             }).then(function successCallBack(response) {
+                var time2 = Date.now();
                 $scope.data.resp = response.data;
+                $scope.data.resTime = time2 - time + " msec";
             });
         };
     });
