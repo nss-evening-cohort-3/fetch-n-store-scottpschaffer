335d0d0e60c6380732033fdf20f8fdd715ac17ed
Scott Schaffer <scottpschaffer@hotmail.com>
2016-10-24 19:27:41 -0700

    Started creating Entity Framework stuff

---
+++ FetchNStore1/FetchNStore1/DAL/FetchStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FetchNStore1.Models;
+using System.Data.Entity;
+
+namespace FetchNStore1.DAL
+{
+    public class FetchStoreContext
+    {
+        public virtual DbSet<Response> Responses { get; set; }
+    }
+}
---
+++ FetchNStore1/FetchNStore1/Models/Response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FetchNStore1.Models
+{
+    public class Response
+    {
+        [Key]
+        public int ResponseId { get; set; }
+
+        [Required]
+        public string StatusCode { get; set; }
+
+        [Required]
+        public string URL { get; set; }
+
+        [Required]
+        public int ResponseTime { get; set; }
+
+        public string HTTPMethod { get; set; }
+
+        public DateTime RequestTime { get; set; }
+    }
+}
